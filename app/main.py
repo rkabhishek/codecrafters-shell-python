d6f9b42cd23f87943154e6216a734cabb61a36ac
Abhishek Kaushik <akaushik079@gmail.com>
2026-01-29 15:35:23 +0530

    Implement a REPL

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    print("$ ", end="", flush=True)
-    cmd = input()
-    sys.stdout.write(f"{cmd}: command not found\n")
+    while True:
+        print("$ ", end="", flush=True)
+        cmd = input()
+        print(f"{cmd}: command not found")
+
 
 
 if __name__ == "__main__":
